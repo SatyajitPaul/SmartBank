d6da1a520a7b31a32533f3e3d53a68173dcc4f78
Satyajit Paul <spaul123@in.ibm.com>
2020-03-18 11:46:40 +0530

    Initial commit

---
+++ app/src/main/java/xyz/satyajit/smartbank/MainActivity.java
@@ -0,0 +1,14 @@
+package xyz.satyajit.smartbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
